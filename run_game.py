ce125fca203233d64bdcc92fbe8e4f1654018696
Liam Sexton <liams4@uw.edu>
2019-06-16 10:31:38 -0700

    Added snake body functionality

--- run_game.py
+++ run_game.py
@@ -1,30 +1,35 @@
-import sys
 import pygame
 from settings import Settings
-from snake import Snake
+from snake import SnakeHead, SnakeBody
+from game_functions import respond_to_input
 
 def run_game():
     pygame.init()
     game_settings = Settings()
 
-    # initialize screen
     screen = pygame.display.set_mode((game_settings.x_dim, game_settings.y_dim))
     pygame.display.set_caption(game_settings.title)
     background_color = [250, 250, 250]
 
-    # create snake
-    snake = Snake(screen, game_settings)
+    snake_head = SnakeHead(screen, game_settings)
+    positions = [[snake_head.rect.centerx, snake_head.rect.centery]]
+    for i in range(game_settings.starting_body_length):
+        positions.append([positions[i][0] - 15, snake_head.rect.centery])
 
+    
     while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-        # check for user input
-        # move snake based on user input
-        # react to move
         screen.fill(background_color)
-        snake.draw_image()
+        respond_to_input(snake_head)
+        snake_head.update(positions)
+        snake_head.draw_image()
+        for position in positions[1:]:
+            
+            body_segment = SnakeBody(screen, game_settings, position[0], position[1])
+            body_segment.draw_image()
+         
+
         
+       
 
         pygame.display.flip()
 
