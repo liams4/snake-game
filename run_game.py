c61cf62e0c23ddff01045340aca80f308542d4f5
Liam Sexton <liams4@uw.edu>
2019-07-08 22:32:24 -0700

    Refactor code

--- run_game.py
+++ run_game.py
@@ -1,47 +1,49 @@
 import pygame
+
 from settings import Settings
 from snake import SnakeHead, SnakeBody
-from game_functions import *
+from game_functions import respond_to_input, check_valid_position
 from food import Food
 
 def run_game():
+    """Driver function for snake-game.
+    """
     pygame.init()
 
     game_settings = Settings()
     screen = pygame.display.set_mode((game_settings.x_dim, game_settings.y_dim))
+    
     pygame.display.set_caption(game_settings.title)
     background_color = game_settings.background_color
-    snake_head = SnakeHead(screen, game_settings)
+    snake_head = SnakeHead(screen)
 
-    positions = [[snake_head.rect.centerx, snake_head.rect.centery]]
-    for i in range(game_settings.length):
-        positions.append([positions[i][0] - 15, snake_head.rect.centery])
+    body_positions = [[snake_head.rect.centerx, snake_head.rect.centery]]
+    body_directions = [[snake_head.xdirection, snake_head.ydirection]]
+    for i in range(game_settings.body_length):
+        body_positions.append([body_positions[i][0] - snake_head.dist_between_body_segments, snake_head.rect.centery])
+        body_directions.append([snake_head.xdirection, snake_head.ydirection])
     
     food = Food(screen)
-    food.draw_image()
     score = 0
 
     while True:
         screen.fill(background_color)
         respond_to_input(snake_head)
-        snake_head.update(positions)
+        snake_head.update(body_positions, body_directions)
+
         snake_head.draw_image()
-        food.draw_image()
-        for position in positions[1:]:
+        food.draw_image()        
+        for position in body_positions[1:]:
             body_segment = SnakeBody(screen, game_settings, position[0], position[1])
             body_segment.draw_image()
 
-        check_valid_position(snake_head, positions)
+        check_valid_position(snake_head, body_positions)
         if pygame.sprite.collide_rect(snake_head, food):
             score += 1
             food = Food(screen)
-            food.draw_image()
-            game_settings.length += 1
-        print(score)
-        print(game_settings.length)
-
+            snake_head.add_length(body_positions, body_directions)
+    
         pygame.display.flip()
-
 
 
 run_game()
