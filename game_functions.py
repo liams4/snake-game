c3e749eef96a5b5b4f58680656cb0ed6e1c2f917
Liam Sexton <liams4@uw.edu>
2019-07-08 22:28:22 -0700

    Add comments

--- game_functions.py
+++ game_functions.py
@@ -1,36 +1,48 @@
-import pygame
+"""This module contains functions that are used to help run snake-game.
+"""
 import sys
+
+import pygame
+
 from settings import Settings
 
-# This class contains functions that are used to help run snake-game.
-
-# Responds to user input.
-def respond_to_input(snake):
+def respond_to_input(snake_head):
+    """Responds to user input.
+    
+    Args:
+        snake (SnakeHead): the head of the snake in snake_game 
+    """
     for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
-                if snake.xdirection != 0:
+                if snake_head.xdirection != 0:
                     if event.key == pygame.K_UP: 
-                        snake.ydirection = -1
-                        snake.xdirection = 0
+                        snake_head.ydirection = -1
+                        snake_head.xdirection = 0
                     elif event.key == pygame.K_DOWN:
-                        snake.ydirection = 1  
-                        snake.xdirection = 0
+                        snake_head.ydirection = 1  
+                        snake_head.xdirection = 0
                 else:
                     if event.key == pygame.K_RIGHT: 
-                        snake.xdirection = 1
-                        snake.ydirection = 0
+                        snake_head.xdirection = 1
+                        snake_head.ydirection = 0
                     elif event.key == pygame.K_LEFT:
-                        snake.xdirection = -1  
-                        snake.ydirection = 0 
+                        snake_head.xdirection = -1  
+                        snake_head.ydirection = 0 
     
 
 def check_valid_position(snake_head, positions):
+    """Checks if the snake is in a valid position.
+    
+    Args:
+        snake_head (SnakeHead): The snake's head in snake game
+        positions (list): positions of each segment of the snake
+    """
     game_settings = Settings()
     if (positions[0] in positions[1:] or 
-            snake_head.rect.centerx < 0 or 
-            snake_head.rect.centerx > game_settings.x_dim or 
-            snake_head.rect.centery < 0 or 
-            snake_head.rect.centery > game_settings.y_dim):
-            sys.exit()
+        snake_head.rect.centerx < 0 or 
+        snake_head.rect.centerx > game_settings.x_dim or 
+        snake_head.rect.centery < 0 or 
+        snake_head.rect.centery > game_settings.y_dim):
+        sys.exit()
