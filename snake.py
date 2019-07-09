4ef38096229aabf895d63c4b97248ef3d4fa81d5
Liam Sexton <liams4@uw.edu>
2019-07-08 22:33:10 -0700

    Add comments

--- snake.py
+++ snake.py
@@ -1,53 +1,94 @@
 import pygame
 from pygame.sprite import Sprite
 
-# This class represents the snake in snake-game.
+from settings import Settings
+
 class SnakeHead(Sprite):
+    """This class represents the snake head in snake-game.
     
-    def __init__(self, screen, snake_settings):
+    Args:
+        Sprite ([type]): [description]
+    """
+    
+    def __init__(self, screen):
+        """Initialize the head of the snake.
+        
+        Args:
+            screen (pygame.Surface): The screen that this snake head appears on.
+        """
         super().__init__()
 
+        game_settings = Settings()
         self.screen = screen
-        self.image = pygame.Surface(snake_settings.size)
-        self.image.fill(snake_settings.head_color)
+        self.image = pygame.Surface(game_settings.size)
+        self.image.fill(game_settings.head_color)
         self.rect = self.image.get_rect()
 
         self.xdirection = 1
         self.ydirection = 0
-        self.speed = 15
+        self.dist_between_body_segments = 15
         
         # Center the snake
         screen_rect = screen.get_rect()
         self.rect.centerx = screen_rect.centerx
         self.rect.centery = screen_rect.centery
 
-    # Draws the snake head on the screen.
     def draw_image(self):
+        """Draws the snake head on the screen.
+        """
         self.screen.blit(self.image, self.rect)
+ 
+    def update(self, body_positions, body_directions):
+        """Updates the position of the snake head.
+        
+        Args:
+            positions (list): positions of each segment of the snake
+            directions (list): current direction of each segment of the snake
+        """
+        self.rect.centerx += self.xdirection * self.dist_between_body_segments
+        self.rect.centery += self.ydirection * self.dist_between_body_segments
+        
+        for i in range(len(body_positions) - 1, 0, -1):
+            body_positions[i] = body_positions[i - 1]
+            body_directions[i] = body_directions[i - 1]
+        body_positions[0] = [self.rect.centerx, self.rect.centery]
+        body_directions[0] = [self.xdirection, self.ydirection]
+    
+    def add_length(self, body_positions, body_directions):
+        """[summary]
+        
+        Args:
+            positions ([type]): [description]
+            directions ([type]): [description]
+        """
+        location = [body_positions[-1][0], body_positions[-1][0] + 1]
+        if body_directions[-1][0] == 1:
+            location = [body_positions[-1][0] - 1, body_positions[-1][0]]
+        elif body_directions[-1][0] == -1:
+            location = [body_positions[-1][0] + 1, body_positions[-1][0]]
+        elif body_directions[-1][1] == 1:
+            location = [body_positions[-1][0], body_positions[-1][0] - 1]
 
-    # Updates the position of the snake.
-    def update(self, positions):
+        body_positions.append(location)
+        body_directions.append([self.xdirection, self.ydirection])
         
-        self.rect.centerx += self.xdirection * self.speed
-        self.rect.centery += self.ydirection * self.speed
         
-        for i in range(len(positions) - 1, 0, -1):
-            positions[i] = positions[i - 1]
-        positions[0] = [self.rect.centerx, self.rect.centery]
-        
+
 class SnakeBody(Sprite):
-
-    def __init__(self, screen, snake_settings, x_pos, y_pos):
+    """This class represents the snake body segment in snake-game."""
+    
+    def __init__(self, screen, game_settings, x_pos, y_pos):
         super().__init__()
 
         self.screen = screen
-        self.image = pygame.Surface(snake_settings.size)
-        self.image.fill(snake_settings.body_color)
+        self.image = pygame.Surface(game_settings.size)
+        self.image.fill(game_settings.body_color)
         self.rect = self.image.get_rect()
 
         self.rect.centerx = x_pos
         self.rect.centery = y_pos
 
-    # Draws the snake body segment on the screen.
     def draw_image(self):
+        """Draws the snake body segment on the screen.
+        """
         self.screen.blit(self.image, self.rect)
