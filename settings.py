2d12b32f8c29bc0c87f24817b80dd92c15887e80
Liam Sexton <liams4@uw.edu>
2019-07-08 22:32:44 -0700

    Add comment

--- settings.py
+++ settings.py
@@ -1,17 +1,16 @@
-# This class contains general settings for snake_game.
 class Settings():
-    
+    """This class contains general settings for snake-game.
+    """
     def __init__(self):
         # Screen settings
-        self.x_dim = 1200
-        self.y_dim = 800
+        self.x_dim = 900
+        self.y_dim = 700
         self.title = 'Snake'
+        self.background_color = [250, 250, 250]
 
-        # Snake sprite settings
-        self.starting_body_length = 3
-        self.size = [10, 10]
+        # Sprite settings
+        self.body_length = 10
+        self.size = [15, 15]
         self.head_color = 250, 20, 20
         self.body_color = 20, 250, 20
-
-        # Other settings
         self.food_color = 20, 20, 250
