140f8d75b03510d5f6a93809534c298dfad310df
Liam Sexton <liams4@uw.edu>
2019-08-25 17:17:37 -0700

    Add ability to play again

--- run_game.py
+++ run_game.py
@@ -1,49 +1,69 @@
+import sys
+
 import pygame
 
 from settings import Settings
-from snake import SnakeHead, SnakeBody
-from game_functions import respond_to_input, check_valid_position
+from snake import SnakeHead, SnakeBodySegment
+from game_functions import (valid_position, add_body_segment, 
+                           update_body_positions_and_directions, check_if_user_wants_to_play_again)
 from food import Food
+from scoreboard import Scoreboard
+from end_of_game_options import PlayAgainButton, EndOfGameMessage
 
-def run_game():
-    """Driver function for snake-game.
+
+def run_game(high_score=0):
+    """Driver function for Snake.
+    
+    Args:
+        high_score (int): The high score of current game session.
+            Defaults to 0.
     """
     pygame.init()
 
     game_settings = Settings()
-    screen = pygame.display.set_mode((game_settings.x_dim, game_settings.y_dim))
-    
     pygame.display.set_caption(game_settings.title)
-    background_color = game_settings.background_color
+    screen = pygame.display.set_mode((game_settings.x_dim, game_settings.y_dim))    
+    
     snake_head = SnakeHead(screen)
-
     body_positions = [[snake_head.rect.centerx, snake_head.rect.centery]]
     body_directions = [[snake_head.xdirection, snake_head.ydirection]]
     for i in range(game_settings.body_length):
         body_positions.append([body_positions[i][0] - snake_head.dist_between_body_segments, snake_head.rect.centery])
         body_directions.append([snake_head.xdirection, snake_head.ydirection])
-    
+
     food = Food(screen)
-    score = 0
-
+    score_board = Scoreboard(screen)
     while True:
-        screen.fill(background_color)
-        respond_to_input(snake_head)
-        snake_head.update(body_positions, body_directions)
+        screen.fill(game_settings.background_color)
+        snake_head.update_direction()
+        snake_head.update_position()
+        update_body_positions_and_directions(snake_head, body_positions, body_directions)
 
-        snake_head.draw_image()
-        food.draw_image()        
+        score_board.draw_to_screen()  
+        snake_head.draw_to_screen()
+        food.draw_to_screen()
         for position in body_positions[1:]:
-            body_segment = SnakeBody(screen, game_settings, position[0], position[1])
-            body_segment.draw_image()
+            body_segment = SnakeBodySegment(screen, position[0], position[1])
+            body_segment.draw_to_screen()
 
-        check_valid_position(snake_head, body_positions)
+        if not valid_position(snake_head, body_positions):
+            break
         if pygame.sprite.collide_rect(snake_head, food):
-            score += 1
+            score_board.add_score()
             food = Food(screen)
-            snake_head.add_length(body_positions, body_directions)
+            add_body_segment(snake_head, body_positions, body_directions)
     
-        pygame.display.flip()
+        pygame.display.flip() # update screen
+   
+    end_of_game_message = EndOfGameMessage(screen, score_board.score, high_score)
+    play_again_button = PlayAgainButton(screen)
+    end_of_game_message.draw_to_screen()
+    play_again_button.draw_to_screen()
+    if score_board.score > high_score:
+        high_score = score_board.score
 
+    play_again = check_if_user_wants_to_play_again(play_again_button)
+    if play_again:
+        run_game(high_score)
 
 run_game()
